43f8e5b3450419aed9424912a11b2f941656aadd
Andrew Ferrazzutti <andrewf@element.io>
2022-09-27 03:12:02 -0400

    Update telemetry support
    - add config to specify a custom telemetry room to join/create
    - render Matrix events in a human-readable format
    - support basic auth for HTTP endpoint
    - use shared IDs for telemetry event & room types
    - refactor for parity with with Synapse telemetry

--- mautrix_telegram/telemetry/service.py
+++ mautrix_telegram/telemetry/service.py
@@ -21,120 +21,227 @@ import asyncio
 import logging
 import time
 
-from aiohttp import ClientSession, hdrs
+from aiohttp import BasicAuth, ClientSession, hdrs
 from attr import dataclass
 
 from mautrix.api import HTTPAPI
 from mautrix.client import ClientAPI
-from mautrix.errors import MNotFound
-from mautrix.types import EventType, RoomID, SerializableAttrs, SerializerError
+from mautrix.errors import MatrixRequestError, MExclusive, MNotFound
+from mautrix.types import (
+    EventType,
+    Format,
+    MessageType,
+    RoomCreateStateEventContent,
+    RoomID,
+    RoomType,
+    SerializableAttrs,
+    SerializerError,
+    TextMessageEventContent,
+)
 
-from .types import TELEMETRY_TYPE, TelemetryData, TelemetryDataRMAU, TelemetryEvent
+from .config import Config, HTTPDestinationConfig, MatrixDestinationConfig
+from .format import telemetry_to_html, telemetry_to_markdown
+from .types import Telemetry as TelemetryEventContent, TelemetryData, TelemetryDataRMAU
 
 if TYPE_CHECKING:
     from ..__main__ import TelegramBridge
 
-TELEMETRY_BASE_TYPE_NAME = f"{TELEMETRY_TYPE}.telemetry"
-TELEMETRY_ROOM_TYPE_NAME = f"{TELEMETRY_BASE_TYPE_NAME}.storage.room"
-TELEMETRY_EVENT_TYPE_NAME = f"{TELEMETRY_BASE_TYPE_NAME}.activity"
 
-TelemetryEventType = EventType.find(TELEMETRY_EVENT_TYPE_NAME, EventType.Class.MESSAGE)
+TelemetryEventType = EventType.find("io.element.ems.telemetry", EventType.Class.MESSAGE)
+
+TelemetryRoomEventType = EventType.find(
+    f"{TelemetryEventType.t}.storage.room", EventType.Class.ACCOUNT_DATA
+)
 
 
 @dataclass
-class TelemetryRoomAccountDataEventContent(SerializableAttrs):
+class TelemetryRoomEventContent(SerializableAttrs):
     room_id: RoomID
 
 
-class TelemetryService:
-    log: logging.Logger = logging.getLogger("mau.telemetry")
+RoomType.TELEMETRY_ROOM = TelemetryRoomEventType.t
+# NOTE: redefining as a global to satisfy static type checks
+TelemetryRoomType: RoomType = RoomType.TELEMETRY_ROOM
 
-    _instance_id: str
+
+class TelemetryService:
+    log: logging.Logger = logging.getLogger("mau.telemetry.service")
+
     _hostname: str
     _matrix_client: ClientAPI
 
-    _endpoint: str
-    _retry_count: int
-    _retry_interval: int
+    _config: Config
 
-    _telemetry_room_id: RoomID | None = None
     _session: ClientSession | None = None
 
     def __init__(self, bridge: TelegramBridge, instance_id: str) -> None:
-        self._instance_id = instance_id
+        if not bridge.config["telemetry.enabled"]:
+            raise ValueError(
+                "TelemetryService cannot be initialized if telemetry is not configured"
+            )
+
         self._hostname = bridge.az.domain
         self._matrix_client = bridge.az.intent
 
-        self._endpoint = bridge.config["telemetry.endpoint"]
-        self._retry_count = bridge.config["telemetry.retry_count"]
-        self._retry_interval = bridge.config["telemetry.retry_interval"]
-        if self._endpoint:
+        self._config = Config(
+            instance_id,
+            MatrixDestinationConfig(
+                bridge.config["telemetry.matrix_destination.room_id_or_alias"],
+                bridge.config["telemetry.matrix_destination.room_creation.options"]
+                if bridge.config["telemetry.matrix_destination.room_creation.enabled"]
+                else None,
+            ),
+            HTTPDestinationConfig(
+                bridge.config["telemetry.http_destination.num_attempts"],
+                bridge.config["telemetry.http_destination.retry_delay"],
+                bridge.config["telemetry.http_destination.submission_url"],
+            )
+            if bridge.config["telemetry.http_destination.enabled"]
+            else None,
+        )
+        if self._config.matrix_destination.room_creation_options is not None:
+            if self._config.matrix_destination.room_id_or_alias and (
+                self._config.matrix_destination.room_id_or_alias.is_room_id
+                or self._config.matrix_destination.room_id_or_alias.domain != self._hostname
+            ):
+                raise ValueError(
+                    '"telemetry.matrix_destination.room_id_or_alias" must refer to a local room alias when '
+                    '"telemetry.matrix_destination.room_creation" is set'
+                )
+
+        if self._config.http_destination:
+            # NOTE: not storing credentials in a config class as they get stored in the ClientSession
             self._session = ClientSession(
                 loop=bridge.loop,
                 headers={
                     hdrs.USER_AGENT: HTTPAPI.default_ua,
                     hdrs.CONTENT_TYPE: "application/json",
                 },
+                auth=BasicAuth(
+                    bridge.config["telemetry.http_destination.credentials.username"],
+                    bridge.config["telemetry.http_destination.credentials.password"],
+                ),
             )
 
     async def _load_storage_room(self) -> RoomID:
-        if self._telemetry_room_id:
-            return self._telemetry_room_id
-
+        remembered_room_id = None
         try:
-            account_data = TelemetryRoomAccountDataEventContent.deserialize(
-                await self._matrix_client.get_account_data(TELEMETRY_ROOM_TYPE_NAME)
-            )
-            room_id = account_data.room_id
+            remembered_room_id = TelemetryRoomEventContent.deserialize(
+                await self._matrix_client.get_account_data(TelemetryRoomEventType)
+            ).room_id
         except (MNotFound, SerializerError):
-            room_id = await self._matrix_client.create_room(
-                creation_content={"type": TELEMETRY_ROOM_TYPE_NAME}
-            )
+            pass
+        except:
+            self.log.exception("Failed to retrieve previously-used telemetry room")
+
+        room_id = None
+        if self._config.matrix_destination.room_id_or_alias:
+            try:
+                room_id = await self._matrix_client.join_room(
+                    self._config.matrix_destination.room_id_or_alias.get(),
+                    max_retries=0,
+                )
+            except MNotFound:
+                if self._config.matrix_destination.room_id_or_alias.is_room_id:
+                    raise
+        elif remembered_room_id:
+            try:
+                room_id = await self._matrix_client.join_room_by_id(remembered_room_id)
+            except Exception as e:
+                self.log.error(
+                    f"Could not join previously-used telemetry room {remembered_room_id}: {e}"
+                )
+
+        if not room_id:
+            if self._config.matrix_destination.room_creation_options is None:
+                raise Exception("Telemetry room creation blocked by config")
+
+            if (
+                self._config.matrix_destination.room_id_or_alias
+                and self._config.matrix_destination.room_id_or_alias.is_room_alias
+            ):
+                alias_localpart = self._config.matrix_destination.room_id_or_alias.localpart
+            else:
+                alias_localpart = None
+            try:
+                room_id = await self._matrix_client.create_room(
+                    alias_localpart,
+                    creation_content=RoomCreateStateEventContent(type=TelemetryRoomType),
+                    **(self._config.matrix_destination.room_creation_options),
+                )
+            except MExclusive as e:
+                raise RuntimeError(
+                    "Failed to create telemetry room with alias "
+                    f'#{alias_localpart}:{self._hostname}". '
+                    "To grant the bridge permission to use this alias, "
+                    "regenerate the bridge registration file."
+                ) from None
+
+        if remembered_room_id and remembered_room_id != room_id:
+            try:
+                await self._matrix_client.leave_room(remembered_room_id)
+            except MatrixRequestError:
+                pass
+            except:
+                self.log.exception("Failed to leave previously-used telemetry room")
+        try:
             await self._matrix_client.set_account_data(
-                TELEMETRY_ROOM_TYPE_NAME, TelemetryRoomAccountDataEventContent(room_id)
+                TelemetryRoomEventType, TelemetryRoomEventContent(room_id).serialize()
             )
-        self._telemetry_room_id = room_id
+        except:
+            self.log.exception("Failed to store telemetry room")
         return room_id
 
     async def send_telemetry(
         self, active_users: int, current_ms: float = time.time() * 1000
     ) -> None:
-        payload = TelemetryEvent(
-            self._instance_id,
-            self._hostname,
-            int(current_ms),
-            TelemetryData(
-                TelemetryDataRMAU(
+        telemetry = TelemetryEventContent(
+            instanceId=self._config.instance_id,
+            hostname=self._hostname,
+            generationTime=int(current_ms),
+            data=TelemetryData(
+                rmau=TelemetryDataRMAU(
                     allUsers=active_users,
-                )
+                ),
             ),
         )
-        self.log.debug(f"Sending telemetry: {payload}")
+        telemetry_json = telemetry.serialize()
+        self.log.debug(f"Sending telemetry: {telemetry_json}")
 
         try:
             room_id = await self._load_storage_room()
-            await self._matrix_client.send_message_event(room_id, TelemetryEventType, payload)
+            human_readable_event_data = TextMessageEventContent(
+                body=telemetry_to_markdown(telemetry),
+                format=Format.HTML,
+                formatted_body=telemetry_to_html(telemetry),
+                msgtype=MessageType.TEXT,
+            )
+            content = human_readable_event_data.serialize()
+            content[TelemetryEventType.t] = telemetry_json
+            await self._matrix_client.send_message(room_id, content)
         except:
             self.log.exception("Failed to record telemetry in Matrix")
 
-        if self._endpoint:
+        if self._config.http_destination:
             assert self._session
-            retries_left = self._retry_count
+            attempts_left = self._config.http_destination.num_attempts
             while True:
                 try:
                     request = self._session.request(
-                        hdrs.METH_POST, self._endpoint, data=payload.serialize()
+                        hdrs.METH_POST,
+                        self._config.http_destination.submission_url,
+                        json=telemetry_json,
                     )
                     async with request as response:
                         response.raise_for_status()
                         break
                 except:
                     self.log.exception("Failed to submit telemetry")
-                    if retries_left > 1:
+                    if attempts_left > 1:
                         self.log.debug(
-                            f"Will retry sending telemetry in {self._retry_interval} seconds"
+                            f"Will retry sending telemetry in {self._config.http_destination.retry_delay} seconds"
                         )
-                        retries_left -= 1
-                        await asyncio.sleep(self._retry_interval)
+                        attempts_left -= 1
+                        await asyncio.sleep(self._config.http_destination.retry_delay)
                     else:
                         break
