2bb78d6d8389179a638a897c41bf24c165a619d2
Andrew Ferrazzutti <andrewf@element.io>
2022-10-18 15:15:40 -0400

    Reformat with black

--- mautrix_telegram/version.py
+++ mautrix_telegram/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = 'f43f43f9'
-version = '0.12.1+dev.f43f43f9'
-linkified_version = '0.12.1+dev.[f43f43f9](https://github.com/mautrix/telegram/commit/f43f43f975c595a50086cc24bdcb0d0f23587641)'
+git_revision = "f43f43f9"
+version = "0.12.1+dev.f43f43f9"
+linkified_version = "0.12.1+dev.[f43f43f9](https://github.com/mautrix/telegram/commit/f43f43f975c595a50086cc24bdcb0d0f23587641)"
